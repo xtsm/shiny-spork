a84704c199f914e93be2a613f25fc3d821d3e278
Nikita.Sechko <sechko2000@mail.ru>
2019-03-02 09:00:12 +0300

    Добавил PriorityWidget

--- include/State.hpp
+++ include/State.hpp
@@ -1,14 +1,32 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <set>
 
 class StateManager;
+class Widget;
 
 class State {
  public:
   State(StateManager& states);
   virtual void Tick() = 0;
-  virtual void Draw(sf::RenderTarget &target) = 0;
-  virtual void ProcessEvents(sf::Window &wnd) = 0;
+  virtual void Draw(sf::RenderTarget& target) = 0;
+  virtual void ProcessEvents(sf::Window& wnd) = 0;
  protected:
+//  Структурка для сотировки Widget по приоритету
+  struct PriorityWidget {
+   public:
+    PriorityWidget(int priority, Widget& widget) : priority_(priority), widget_(widget) {}
+    friend bool operator<(State::PriorityWidget lhs, State::PriorityWidget rhs) {
+      return lhs.priority_ < rhs.priority_;
+    }
+   private:
+    int priority_;
+    Widget& widget_;
+  };
+//  Непосредственно Widgetы, отсортированные по приоритету
+  std::set<PriorityWidget> WidgetPriorityQueue;
   StateManager& states;
 };
+
+
+
