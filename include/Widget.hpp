bd583a7932edd0b240a6b820b65b23737c8253c9
Nikita.Sechko <sechko2000@mail.ru>
2019-03-02 08:39:17 +0300

    Добавил Widget

--- include/Widget.hpp
+++ include/Widget.hpp
@@ -1,1 +1,11 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include <State.hpp>
+
+class Widget{
+ public:
+  explicit Widget(State& state);
+  virtual void ProcessEvent(sf::Event& event)  = 0;
+ protected:
+  State& state;
+};
