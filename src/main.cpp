9e688129a08cc682f11bce68a9b4418b4a0307db
Stanislau Tsitsianok <tsmx@tut.by>
2019-03-31 17:53:03 +0300

    Window shouldn't appear offscreen now

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ int main() {
   sf::RenderWindow wnd(sf::VideoMode(800,600),"",
       sf::Style::Titlebar);
   wnd.setVerticalSyncEnabled(true);
+  wnd.setPosition(sf::Vector2i(400,300));
   StateManager states;
   while (!states.Closed()) {
     wnd.clear();
