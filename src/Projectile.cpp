a000df8a7f3452918ba41c0f4ee3a7c9cc0f5c40
Nikita.Sechko <sechko2000@mail.ru>
2019-04-12 15:48:32 +0300

    Добавил поворот снаряда

--- src/Projectile.cpp
+++ src/Projectile.cpp
@@ -1,5 +1,9 @@
+#include <cmath>
 #include "entity/Projectile.h"
 #include "StateManager.h"
+
+const double pi = 3.14159265358979323846;
+const double from_radian_to_degree = 180 / pi;
 
 Projectile::Projectile(State& state, std::shared_ptr<Enemy> aim,
                        int x, int y, int damage, int speed) :
@@ -20,6 +24,7 @@ void Projectile::Pointing() {
   } else {
     d *= speed_ * speed_ / d.Length();
     position_ += d;
+    sprite_.setRotation((atan2(d.y, d.x) + pi) * from_radian_to_degree);
     sprite_.setPosition(position_.x, position_.y);
   }
 }
