dc2c7ae16b10a5555542e4c4b3c7a399c17f4a56
nikkita <nikkita1267@gmail.com>
2019-04-14 10:27:09 +0300

    Remake enemy move

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -10,19 +10,38 @@
 #include "utility/Direction.h"
 
 void Enemy::DoMove() {
-  std::vector<Direction> possible_moves = GetAvailableMoves(x_, y_);
-  // Randomly choose move
-  if (possible_moves.empty()) return;
+  std::vector<std::pair<Tile, Direction>> possible_moves =
+      GetAvailableMoves(x_, y_);
+  if (current_tile_ == desination_tile_) {
+    if (possible_moves.empty()) return;
 
-  std::mt19937 generator(std::chrono::steady_clock::now().time_since_epoch().count());
-  std::uniform_int_distribution<int> distribution(
-      0, static_cast<int>(possible_moves.size() - 1));
-  int choose_move = distribution(generator);
-  DoMove(possible_moves[choose_move]);
+    std::mt19937 generator(
+        static_cast<unsigned int>(std::chrono::steady_clock::now()
+            .time_since_epoch().count()));
+
+    std::uniform_int_distribution<int> distribution_of_index(
+        0, static_cast<int>(possible_moves.size() - 1));
+
+    auto[new_destination, direction] =
+    possible_moves[distribution_of_index(generator)];
+    desination_tile_ = new_destination;
+    std::cerr << "Change direction from " << direction_of_move_
+              << " to " << direction << std::endl;
+    direction_of_move_ = direction;
+  }
+
+  if (state_.GetStateManager().game_ptr_->GetMap()->
+      IsMoveAvailable(direction_of_move_, x_, y_)) {
+    DoMove(direction_of_move_);
+  } else {
+    desination_tile_ = current_tile_;
+    if (!possible_moves.empty()) {
+      DoMove();
+    }
+  }
 }
 
 void Enemy::DoMove(const Direction& direction) {
-  move_direction_ = direction;
   switch (direction) {
     case Direction::North:
       position_.y -= speed_;
@@ -49,12 +68,18 @@ void Enemy::DoMove(const Direction& direction) {
   }
 
   sprite_.setPosition(static_cast<float>(position_.x), static_cast<float>(position_.y));
+  Tile tile_after_move = state_.GetStateManager().game_ptr_->
+      GetMap()->GetTile(x_, y_);
+  ChangeDirectionTile(tile_after_move);
 }
 
 Enemy::Enemy(double health, double speed, double x, double y,
-             const Direction& move_direction, State& state, int priority)
+             const Tile& current_tile, const Direction& direction,
+             State& state, int priority)
     : Entity(state, DrawPriority(priority, this)),
-      move_direction_(move_direction),
+      current_tile_(current_tile),
+      direction_of_move_(direction),
+      desination_tile_(current_tile),
       health_(health),
       speed_(speed),
       power_(0),
@@ -65,7 +90,6 @@ Enemy::Enemy(double health, double speed, double x, double y,
   y_ = static_cast<int>(y);
   sprite_.setPosition(static_cast<float>(x), static_cast<float>(y));
   sprite_.setTextureRect(sf::IntRect(5, 5, 27, 40));
-  DoMove(move_direction_);
 }
 
 EnemyCreator::EnemyCreator(State& state)
@@ -94,49 +118,51 @@ void EnemyCreator::CreateSomeEnemies(int64_t count) {
 
   for (int64_t i = 0; i < count; ++i) {
     int type = distribution_of_type(generator);
-    EnemyHealthType health;
+    EnemyType health;
     if (type >= 0 && type < 5) {
-      health = EnemyHealthType::VeryHigh;
+      health = EnemyType::VeryHigh;
     } else if (type >= 5 && type < 15) {
-      health = EnemyHealthType::High;
+      health = EnemyType::High;
     } else if (type >= 15 && type < 30) {
-      health = EnemyHealthType::Middle;
+      health = EnemyType::Middle;
     } else {
-      health = EnemyHealthType::Low;
+      health = EnemyType::Low;
     }
 
     auto point_of_spawn_with_direction = spawn_points_[distribution_of_points(generator)];
-    state_.GetStateManager().game_ptr_->AddNewEnemy(GetHealthFromType(health), GetSpeedByType(health),
-                                                    point_of_spawn_with_direction.first.x * 60,
-                                                    point_of_spawn_with_direction.first.y * 60,
-                                                    point_of_spawn_with_direction.second);
+    state_.GetStateManager().
+        game_ptr_->AddNewEnemy(
+        GetHealthFromType(health), GetSpeedByType(health),
+        point_of_spawn_with_direction.first.x * 60,
+        point_of_spawn_with_direction.first.y * 60,
+        point_of_spawn_with_direction.second);
   }
 }
 
-int EnemyCreator::GetHealthFromType(const EnemyHealthType& type) {
+int EnemyCreator::GetHealthFromType(const EnemyType& type) {
   switch (type) {
-    case EnemyHealthType::VeryHigh:
+    case EnemyType::VeryHigh:
       return 1000;
-    case EnemyHealthType::High:
+    case EnemyType::High:
       return 500;
-    case EnemyHealthType::Middle:
+    case EnemyType::Middle:
       return 250;
-    case EnemyHealthType::Low:
+    case EnemyType::Low:
       return 100;
     default:
       return 0;
   }
 }
 
-double EnemyCreator::GetSpeedByType(const EnemyHealthType& type) {
+double EnemyCreator::GetSpeedByType(const EnemyType& type) {
   switch (type) {
-    case EnemyHealthType::VeryHigh:
+    case EnemyType::VeryHigh:
       return 0.2;
-    case EnemyHealthType::High:
+    case EnemyType::High:
       return 0.4;
-    case EnemyHealthType::Middle:
+    case EnemyType::Middle:
       return 0.8;
-    case EnemyHealthType::Low:
+    case EnemyType::Low:
       return 1;
     default:
       return 0;
@@ -166,26 +192,34 @@ void Enemy::DoDamage(Enemy& other_entity) {
   other_entity.DecreaseHealth(power_);
 }
 
-std::vector<Direction> Enemy::GetAvailableMoves(int x, int y) const {
-  std::vector<Direction> available_moves;
+std::vector<std::pair<Tile, Direction>> Enemy::GetAvailableMoves(int x, int y) const {
+  std::vector<std::pair<Tile, Direction>> available_moves;
 
   std::shared_ptr<Map> map = state_.GetStateManager().game_ptr_->GetMap();
 
   if (map->IsMoveAvailable(Direction::North, x, y)) {
-    available_moves.push_back(Direction::North);
+    available_moves.emplace_back(map->GetTile(x / 60, y / 60 - 1), Direction::North);
   }
 
   if (map->IsMoveAvailable(Direction::West, x, y)) {
-    available_moves.push_back(Direction::West);
+    available_moves.emplace_back(map->GetTile(x / 60 - 1, y / 60), Direction::West);
   }
 
   if (map->IsMoveAvailable(Direction::East, x, y)) {
-    available_moves.push_back(Direction::East);
+    available_moves.emplace_back(map->GetTile(x / 60 + 1, y / 60), Direction::East);
   }
 
   if (map->IsMoveAvailable(Direction::South, x, y)) {
-    available_moves.push_back(Direction::South);
+    available_moves.emplace_back(map->GetTile(x / 60, y / 60 + 1), Direction::South);
   }
 
   return available_moves;
 }
+
+void Enemy::ChangeDirectionTile(const Tile& new_tile) {
+  current_tile_ = new_tile;
+}
+
+//bool Enemy::IsMoveHasChangedTile(const Tile& tile_after_move) {
+//  return tile_after_move == desination_tile_;
+//}
