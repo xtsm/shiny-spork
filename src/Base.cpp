b70d440f619489cab3aac1fee42ce8b88dc14b74
Nikita.Sechko <sechko2000@mail.ru>
2019-04-29 23:23:54 +0300

    Редизайн меню базы, Прозрачный фон базы.

--- src/Base.cpp
+++ src/Base.cpp
@@ -4,7 +4,7 @@
 Base::Base(State& state, const DrawPriority& priority, int health, const Tile& top)
     : Entity(state, priority, health, health), top_left_(top) {
   sprite_.setPosition(480, 0);
-  icon_sprite_.setPosition(650, 125);
+  icon_sprite_.setPosition(630, 220);
 }
 
 std::vector<sf::Text> Base::GetInfo() const {
@@ -12,9 +12,9 @@ std::vector<sf::Text> Base::GetInfo() const {
       .GetOrLoadResource("assets/font/default.ttf");
 
   sf::Text name("Main base", font);
-  InitText(name, 650, 250, 15, sf::Color::Red);
-  sf::Text health_info(std::to_string(health_), font);
-  InitText(health_info, 650, 275, 15);
+  InitText(name, 650, 200, 15, sf::Color::Red);
+  sf::Text health_info("Health: " + std::to_string(health_), font);
+  InitText(health_info, 620, 340, 15);
 
   return {name, health_info};
 }
